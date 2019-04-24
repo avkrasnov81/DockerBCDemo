152f4d15cfb23f76b3f59279b09c7f393b6c0f5d
Andrey Krasnov <akrasnov@navicons.ru>
2019-04-24 07:54:48 +0300

    Исправил создание контейнера

--- Docker_Install_Container_BC.ps1
+++ Docker_Install_Container_BC.ps1
@@ -11,7 +11,8 @@ $Password = ConvertTo-SecureString -String $pass -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $env:USERNAME, $Password
 
 New-NavContainer -containerName $MyContainer `
-    -accept_eula -updatehosts `
+    -accept_eula `
+    -updatehosts `
     -auth NavUserPassword -Credential $credential `
     -imageName $ImageName `
     -includeCSide `
